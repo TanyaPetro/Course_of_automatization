3ccdeb8df489c32bc3a18382a5e3980afe025f79
TanyaPetro <tanlozhkina22@gmail.com>
2024-07-22 20:46:36 +0400

    add control work 2

--- lesson6.1/calc_test.py
+++ lesson6.1/calc_test.py
@@ -1,44 +1,45 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
 
-driver.maximize_window()
+def main(driver):
+    driver.maximize_window()
 
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
-    )
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+        )
 
-timer = '45'
+    timer = '45'
 
-delay = driver.find_element(
-    By.XPATH, "//input[contains(@id, 'delay')]"
-    )
-delay.clear()
-delay.send_keys(timer)
+    delay = driver.find_element(
+        By.XPATH, "//input[contains(@id, 'delay')]"
+        )
+    delay.clear()
+    delay.send_keys(timer)
 
-driver.find_element(
-    By.XPATH, "//span[text()='7']"
-    ).click()
+    driver.find_element(
+        By.XPATH, "//span[text()='7']"
+        ).click()
 
-driver.find_element(
-    By.XPATH, "//span[text()='+']"
-    ).click()
+    driver.find_element(
+        By.XPATH, "//span[text()='+']"
+        ).click()
 
-driver.find_element(
-    By.XPATH, "//span[text()='8']"
-    ).click()
+    driver.find_element(
+        By.XPATH, "//span[text()='8']"
+        ).click()
 
-driver.find_element(
-    By.XPATH, "//span[text()='=']"
-    ).click()
+    driver.find_element(
+        By.XPATH, "//span[text()='=']"
+        ).click()
 
-sleep(int(timer))
+    sleep(int(timer))
 
-result = driver.find_element(
-    By.XPATH, "//div[contains(@class, 'screen')]"
-    ).text
-
-assert result == '15'
-
-driver.quit()
+def test_calc():
+    driver = webdriver.Chrome()
+    main(driver)
+    result = driver.find_element(
+        By.XPATH, "//div[contains(@class, 'screen')]"
+        ).text
+    assert result == '15'
+    driver.quit()
--- lesson6.1/form_fill_test.py
+++ lesson6.1/form_fill_test.py
@@ -1,73 +1,75 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome()
 
-driver.maximize_window()
+def main(driver):
+    
+    driver.maximize_window()
 
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
-    )
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+        )
 
-first_name = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'first-name')]"
-    )
-first_name.send_keys("Иван")
+    first_name = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'first-name')]"
+        )
+    first_name.send_keys("Иван")
 
-last_name = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'last-name')]"
-    )
-last_name.send_keys("Петров")
+    last_name = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'last-name')]"
+        )
+    last_name.send_keys("Петров")
 
-address = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'address')]"
-    )
-address.send_keys("Ленина, 55-3")
+    address = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'address')]"
+        )
+    address.send_keys("Ленина, 55-3")
 
-email = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'e-mail')]"
-    )
-email.send_keys("test@skypro.com")
+    email = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'e-mail')]"
+        )
+    email.send_keys("test@skypro.com")
 
-phone = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'phone')]"
-    )
-phone.send_keys("+7985899998787")
+    phone = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'phone')]"
+        )
+    phone.send_keys("+7985899998787")
 
-city = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'city')]"
-    )
-city.send_keys("Москва")
+    city = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'city')]"
+        )
+    city.send_keys("Москва")
 
-country = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'country')]"
-    )
-country.send_keys("Россия")
+    country = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'country')]"
+        )
+    country.send_keys("Россия")
 
-position = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'job-position')]"
-    )
-position.send_keys("QA")
+    position = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'job-position')]"
+        )
+    position.send_keys("QA")
 
-company = driver.find_element(
-    By.XPATH, "//input[contains(@name, 'company')]"
-    )
-company.send_keys("SkyPro")
+    company = driver.find_element(
+        By.XPATH, "//input[contains(@name, 'company')]"
+        )
+    company.send_keys("SkyPro")
 
-driver.find_element(
-    By.XPATH, "//button[contains(@class, 'btn')]"
-    ).click()
+    driver.find_element(
+        By.XPATH, "//button[contains(@class, 'btn')]"
+        ).click()
 
-zip_color = (driver.find_element(
-    By.XPATH, "//div[contains(@id, 'zip-code')]"
-    ).value_of_css_property("color"))
-assert zip_color == 'rgba(132, 32, 41, 1)'
-
-success_elements = driver.find_elements(
-    By.XPATH, "//div[contains(@class, 'alert-success')]"
-    )
-
-for elem in success_elements:
-    assert (elem.value_of_css_property("color")) == 'rgba(15, 81, 50, 1)'
-
-
-driver.quit()
+def test_form_fill():
+    driver = webdriver.Chrome()
+    main(driver)
+    zip_color = (driver.find_element(
+        By.XPATH, "//div[contains(@id, 'zip-code')]"
+        ).value_of_css_property("color"))
+    assert zip_color == 'rgba(132, 32, 41, 1)'
+    
+    success_elements = driver.find_elements(
+        By.XPATH, "//div[contains(@class, 'alert-success')]"
+        )
+    for elem in success_elements:
+        assert (elem.value_of_css_property("color")) == 'rgba(15, 81, 50, 1)'
+    driver.quit()
+    
